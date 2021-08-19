5f58f3cde992a73659281ffaf4a7de5c1619373e
Adhav712 <Adhav712@gmail.com>
2021-08-19 13:57:37 +0530

    testing

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -6,11 +6,7 @@ import SmartbrainlogoReworked1 from './SmartbrainlogoReworked1.png'
 
 const Logo = () => {
     return(
-<<<<<<< HEAD
         <div className = 'logo'>
-=======
-        <div classname = 'logo'>
->>>>>>> 018a464ba83f70dba29435deac302cf8934002b3
          <Tilt className="Tilt" options={{ max : 35,speed: 300 }} style={{ height: 185, width: 200}} >
           <div className="Tilt-inner"><img className = 'images' alt ='' src = {SmartbrainlogoReworked1}></img> </div>
          </Tilt>
